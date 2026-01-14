4934b145654a0d1cfb5885dea452ad972afefe82
prajwalmane45 <prajwalmane74@gmail.com>
2026-01-14 20:09:15 +0530

    switch backend to mysql

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,12 @@
+// src/config/db.js
 const mysql = require("mysql2/promise");
 
-const db = mysql.createPool({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT),
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
@@ -13,12 +14,12 @@ const db = mysql.createPool({
 
 (async () => {
   try {
-    const connection = await db.getConnection();
+    const conn = await pool.getConnection();
     console.log("✅ Connected to MySQL (Railway)");
-    connection.release();
+    conn.release();
   } catch (err) {
     console.error("❌ MySQL connection error:", err);
   }
 })();
 
-module.exports = db;
+module.exports = pool;
